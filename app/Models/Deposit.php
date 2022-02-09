8e8d04e0323cca6d0a51d053095b4bb7ea051dee
Omer-alba <o.albadry@foxxum.com>
2022-02-09 22:51:44 +0100

    [CREATE] Model Deposit

--- app/Models/Deposit.php
+++ app/Models/Deposit.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Deposit extends Model
 {
     use HasFactory;
+
+    public function next(){
+        return $this->join('parent_child','key','=','parent_id')->first('child_id');
+    }
 }
