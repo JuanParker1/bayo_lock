1c84767e3c0243848498a66d6ae5ba636ee5dfae
Omer-alba <o.albadry@foxxum.com>
2022-03-09 23:02:41 +0100

    [EXTEND] extend new routes for "Models/Trade", "Models/Investor" and "Models/deposit"

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,18 @@ Route::get('/', function () {
 
 Route::get('/test', [\App\Http\Controllers\testController::class,'index']);
 Route::get('/portfolio', [\App\Http\Controllers\testController::class,'portfolio']);
+
+Route::get('/create', [App\Http\Controllers\TradeController::class, 'create']);
+Route::post('/create', [App\Http\Controllers\TradeController::class, 'store']);
+
+Route::get('/investor', [App\Http\Controllers\InvestorController::class, 'index']);
+Route::get('/investor/create', [App\Http\Controllers\InvestorController::class, 'create']);
+Route::post('/investor/create', [App\Http\Controllers\InvestorController::class, 'store']);
+Route::get('/investor/{id}/contracts', [App\Http\Controllers\InvestorController::class, 'showContracts']);
+Route::get('/investor/{id}/create-contract', [App\Http\Controllers\InvestorController::class, 'createContract']);
+
+Route::get('/deposit/create', [App\Http\Controllers\DepositsController::class, 'create']);
+Route::post('/deposit/create', [App\Http\Controllers\DepositsController::class, 'store']);
+
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
