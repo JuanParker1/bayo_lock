6525adc65850f862450d6962f190360112b5d2ee
Omer-alba <o.albadry@foxxum.com>
2022-04-19 17:24:42 +0200

    [NEW] create new table and model for cryptocurrency

---
+++ database/migrations/2022_03_24_191131_create_cryptocurrencies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCryptocurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cryptocurrencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('crypto_id');
+            $table->string('name');
+            $table->string('symbol');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cryptocurrencies');
+    }
+}
