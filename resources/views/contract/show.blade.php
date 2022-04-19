e5b35e4a932ac1172fbfa032763948e588350da7
Omer-alba <o.albadry@foxxum.com>
2022-04-19 17:30:41 +0200

    [FIX] extend style to own file which is include in view

--- resources/views/contract/show.blade.php
+++ resources/views/contract/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/contract/{!! $contract->id !!}/create-trade">create new trade</a>
+    <link href="{{ asset('css/show-trades.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/modal-style.css') }}" rel="stylesheet">
     {{--    <livewire:show-currency :contract_id="$contract->id"/>--}}
+
+    <a href="/contract/{!! $contract->id !!}/create-trade">create new trade</a>
     @foreach($trades as $trade)
         <livewire:show-trades :trade="$trade"/>
     @endforeach
+
 @endsection
