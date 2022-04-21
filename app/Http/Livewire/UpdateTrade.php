4342dc09544729c47b5f2382071d9904dc131510
Omer-alba <o.albadry@foxxum.com>
2022-04-22 01:55:46 +0200

    [CREATE] logic for Controller FormMessage [CREATE] create form message and update trade view

--- app/Http/Livewire/UpdateTrade.php
+++ app/Http/Livewire/UpdateTrade.php
@@ -12,14 +12,56 @@ class UpdateTrade extends Component
     public $summed;
     public $img;
     public $modelTrade;
+    public $sum;
+
+    // FORM ELEMENTS
+    public $formOrderDay;
+    public $formSummed = 0;
+
+    // ERROR MESSAGE
+    public $formMessageSummed;
+    public $formMessageOrderDay;
 
     public function mount()
     {
         $this->modelTrade = Trade::find($this->tradeId);
     }
 
+    public function update()
+    {
+        //check if the number is smaller than the original
+        if ($this->summed < $this->formSummed) {
+            $this->formMessageSummed = 'it has to be smaller than the total amount!';
+            return false;
+        }
+
+        $trade = Trade::find($this->tradeId);
+
+        if (!empty($this->formOrderDay)) {
+            $trade['order-day'] = $this->formOrderDay;
+        }
+
+        $trade['total-currency'] = $this->summed - $this->formSummed;
+
+        $trade->update();
+
+        // if total-currency is equal to zero, delete whole trade.
+        if ($trade['total-currency'] === 0) {
+            $trade->delete();
+        }
+
+        $this->emitUp('closeEditModal');
+    }
+
     public function render()
     {
+        if ($this->summed < $this->formSummed) {
+            $this->formMessageSummed = 'it has to be smaller than the total value!';
+        }
+
+        if ($this->formSummed > 0) {
+            $this->sum = $this->summed - $this->formSummed;
+        }
 
         return view('livewire.update-trade');
     }
--- resources/views/livewire/update-trade.blade.php
+++ resources/views/livewire/update-trade.blade.php
@@ -1,83 +1,94 @@
 <div>
     <div class="blur-container" wire:click.prevent="$emitUp('closeEditModal')">
-        <div class="block">
-            <div class="block-modal" wire:click.stop="">
+        <form wire:submit.prevent="update">
+            <div class="block">
+                <div class="block-modal" wire:click.stop="">
 
-                <div class="modal-edit-layout">
-                    <div class="modal-header">
-                        <div class="modal-icon img">
-                            <img src="{{$img}}">
-                        </div>
-                        <div class="modal-header-text">
-                            {{ $modelTrade->cryptocurrency->name }}
-                        </div>
-                    </div>
-
-                    <div class="modal-body">
-                        <div class="modal-body-header">
-                            <div class="modal-edit-header">current values</div>
-                            <div class="modal-edit-header">to change values</div>
+                    <div class="modal-edit-layout">
+                        <div class="modal-header">
+                            <div class="modal-icon img">
+                                <img src="{{$img}}">
+                            </div>
+                            <div class="modal-header-text">
+                                {{ $modelTrade->cryptocurrency->name }}
+                            </div>
                         </div>
 
-                        <div class="modal-body-content">
-                            {{-- date --}}
-                            <div class="modal-body-tr">
-                                <div class="modal-edit-body">
-                                    <div class="edit-body-icon x-large">
-                                        <i class="bi bi-calendar-week"></i>
-                                    </div>
-                                    <div class="edit-body-text large">
-                                        {{ date('d M. Y G:i' ,strtotime($modelTrade['order-day'])) }}
-                                    </div>
-                                </div>
-
-                                <div class="modal-edit-body">
-                                    <div class="edit-body-icon x-large">
-                                        <i class="bi bi-pen"></i>
-                                    </div>
-                                    <div class="edit-body-text large item-input">
-                                        <input type="datetime-local" value="{{$orderDay}}">
-                                    </div>
-                                </div>
+                        <div class="modal-body">
+                            <div class="modal-body-header">
+                                <div class="modal-edit-header">current values</div>
+                                <div class="modal-edit-header">to change values</div>
                             </div>
 
-                            {{-- currency --}}
-                            <div class="modal-body-tr">
-                                <div class="modal-edit-body">
-                                    <div class="edit-body-icon x-large">
-                                        <i class="bi bi-stack"></i>
-                                        <sub>Amount</sub>
+                            <div class="modal-body-content">
+                                {{-- date --}}
+                                <div class="modal-body-tr">
+                                    <div class="modal-edit-body">
+                                        <div class="edit-body-icon x-large">
+                                            <i class="bi bi-calendar-week"></i>
+                                        </div>
+                                        <div class="edit-body-text large">
+                                            {{ date('d M. Y G:i' ,strtotime($modelTrade['order-day'])) }}
+                                        </div>
                                     </div>
-                                    <div class="edit-body-text large">
-                                        {{ $summed }}
+
+                                    <div class="modal-edit-body">
+                                        <div class="edit-body-icon x-large">
+                                            <i class="bi bi-pen"></i>
+                                        </div>
+                                        <div class="edit-body-text large item-input">
+                                            <input wire:model="formOrderDay" type="datetime-local" value="{{$orderDay}}">
+                                            @if($formMessageOrderDay)
+                                                <livewire:form-message :message="$formMessageSummed" />
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div class="modal-edit-body">
-                                    <div class="edit-body-icon x-large">
-                                        <i class="bi bi-pen"></i>
-                                        <sub>enter the number which has to be reduced</sub>
+                                {{-- currency --}}
+                                <div class="modal-body-tr">
+                                    <div class="modal-edit-body">
+                                        <div class="edit-body-icon x-large">
+                                            <i class="bi bi-stack"></i>
+                                            <sub>Amount</sub>
+                                        </div>
+                                        <div class="edit-body-text large">
+                                            {{ $summed }}
+                                            @if($formSummed > 0)
+                                                <sub>it remains {{ $sum }}</sub>
+                                            @endif
+                                        </div>
                                     </div>
-                                    <div class="edit-body-text large item-input">
-                                        <input type="number" accept="any" placeholder="{{ $summed }}">
+
+                                    <div class="modal-edit-body">
+                                        <div class="edit-body-icon x-large">
+                                            <i class="bi bi-pen"></i>
+                                            <sub>enter the number which has to be reduced</sub>
+                                        </div>
+                                        <div class="edit-body-text large item-input">
+                                            <input wire:model="formSummed" inputmode="numeric" type="number" step="any" placeholder="{{ $summed }}">
+                                            @if($formMessageSummed)
+                                                <livewire:form-message :message="$formMessageSummed" />
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
+
                             </div>
+                        </div>
 
+                        <div class="modal-footer trade-btn-area">
+                            <div class="item">
+                                <button class="btn-action btn-back">return</button>
+                            </div>
+                            <div class="item">
+                                <button class="btn-action btn-store">store</button>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="modal-footer trade-btn-area">
-                        <div class="item">
-                            <button class="btn-action btn-back">return</button>
-                        </div>
-                        <div class="item">
-                            <button class="btn-action btn-store">store</button>
-                        </div>
-                    </div>
                 </div>
-
             </div>
-        </div>
+        </form>
     </div>
 </div>
