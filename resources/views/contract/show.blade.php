1748057efefadd58a74c70b11122dc992989d0c6
Omer-alba <o.albadry@foxxum.com>
2022-04-22 00:00:30 +0200

    [EXTEND] binding the style sheets

--- resources/views/contract/show.blade.php
+++ resources/views/contract/show.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <link href="{{ asset('css/show-trades.css') }}" rel="stylesheet">
     <link href="{{ asset('css/modal-style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/modal-edit.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/trade-create.css') }}" rel="stylesheet">
     {{--    <livewire:show-currency :contract_id="$contract->id"/>--}}
 
     <a href="/contract/{!! $contract->id !!}/create-trade">create new trade</a>
