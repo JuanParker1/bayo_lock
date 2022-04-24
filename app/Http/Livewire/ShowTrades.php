1666e80285c87436771447e1e41e5fe66218ac93
Omer-alba <o.albadry@foxxum.com>
2022-04-24 22:32:01 +0200

    [FIX] clean code adding comments and give them space [FIX] remove bool variables that not more needed [FIX] seperate collapse handler to a own function [EXTEND] delete function adding to clear nearly all include variable

--- app/Http/Livewire/ShowTrades.php
+++ app/Http/Livewire/ShowTrades.php
@@ -11,16 +11,14 @@ class ShowTrades extends Component
 
     public $trades;
     public $tradesKeys = '';
+    public $isCollective;
     public $tradesLivePrices = [];
     public $tradesLiveBalance = [];
 
     public $showCollective = [];
     public $collapseClasses;
 
-    public $openModal = false;
-    public $editAble = false;
-
-    protected $listeners = ['openModal'];
+    protected $listeners = ['openModal', 'delete'];
 
     public function mount()
     {
@@ -29,12 +27,14 @@ class ShowTrades extends Component
         foreach ($this->trades as $key => &$trade) {
             $trade['img'] = $trade['imgUrl'] ?? $cryptoService->getCryptoImage($trade['cryptoId']);
             $trade['class'] = $trade['countOfCollective'] > 1 ? ($trade['countOfCollective'] > 2 ? 'card-block-multiple' : 'card-block-extend') : '';
+            $trade['balance'] = null;
 
             $this->tradesKeys = array_key_last($this->trades) === $key ? $this->tradesKeys . $key : $this->tradesKeys . $key . ',';
             $this->tradesLiveBalance[$key] = null;
             $this->showCollective[$key] = null;
             $this->collapseClasses[$key] = $trade['class'];
         }
+
         $this->refreshPrices();
     }
 
@@ -55,12 +55,7 @@ class ShowTrades extends Component
     public function extend($cryptoId)
     {
         // check if any collapsed is open, close it.
-        foreach ($this->showCollective as $key => &$collective) {
-            if ($cryptoId == $key) continue;
-            $collective = false;
-            $this->collapseClasses[$key] = $this->trades[$key]['class'];
-        }
-
+        $this->collapseRollBack($cryptoId);
         $this->showCollective[$cryptoId] = $this->showCollective[$cryptoId] == true ? false : true;
 
         // toggle class for view
@@ -72,38 +67,87 @@ class ShowTrades extends Component
         $this->collapseClasses[$cryptoId] = $this->trades[$cryptoId]['class'];
     }
 
-    public function delete($id)
+    public function delete($id, $cryptoId = null)
     {
         $idList = explode(',', $id);
-        Trade::find($idList)->each->delete();
-        $trade = null;
+        $model = Trade::find($idList);
+        $model->each->delete();
+
+        // refresh if it is possible
+        $this->trades[$cryptoId] = $this->refreshTrades($cryptoId);
+        if ($this->trades[$cryptoId] === null) {
+            // remove the cryptoId from tradeKeys
+            $this->tradesKeys = str_replace([',' . $cryptoId, $cryptoId], '', $this->tradesKeys);
+            unset($this->trades[$cryptoId]);
+            unset($this->collapseClasses[$cryptoId]);
+            return null;
+        }
+
+        //adding class to new column
+        $this->trades[$cryptoId]['class'] = $this->trades[$cryptoId]['countOfCollective'] > 1 ? ($this->trades[$cryptoId]['countOfCollective'] > 2 ? 'card-block-multiple' : 'card-block-extend') : '';
+
+        // if collapse is opend close it
+        if ($this->trades[$cryptoId]['isCollective']) {
+            $this->collapseRollBack(false);
+        }
     }
 
-    public function decrease()
+    protected function collapseRollBack($cryptoId = null)
     {
-        $this->editAble = True;
+        foreach ($this->showCollective as $key => &$collective) {
+            if ($cryptoId == $key) continue;
+            $collective = false;
+
+            $this->collapseClasses[$key] = $this->trades[$key]['class'];
+        }
     }
 
-    public function closeModal()
-    {
-        $this->openModal = false;
-    }
-
-    public function openModal($type, $cryptoId)
+    public function openModal($type, $cryptoId, $tradeId = null)
     {
         if ($type === 'info') {
+
+            if ($tradeId) {
+                $trades = $this->trades[$cryptoId];
+                $trades['modelId'] = $tradeId;
+            }
+
             $this->emit('openModal', 'trade-modal', [
-                'trade' => $this->trades[$cryptoId],
+                'trade' => $trades ?? $this->trades[$cryptoId],
                 'liveBalance' => $this->tradesLiveBalance[$cryptoId],
                 'livePrice' => $this->tradesLivePrices[$cryptoId]['eur'],
-                'showTradeInfo' => true
+                'showTradeInfo' => true,
+            ]);
+        } elseif ($type === 'edit') {
+            $this->emit('openModal', 'update-trade', [
+                'tradeId' => $this->trades[$cryptoId]['id'],
+                'isCollective' => $this->trades[$cryptoId]['isCollective']
             ]);
         }
     }
 
-    public function closeEditModal()
+    public function refreshTrades($cryptoId)
     {
-        $this->editAble = false;
+        $cryptoService = new CryptoService();
+        $ids = $this->trades[$cryptoId]['collectiveIds'];
+        $trades = Trade::find($ids)->first();
+
+        if (!$trades) {
+            return null;
+        }
+//        $result = $cryptoService->groupByCryptoId($trades);
+//
+//        if ($result) {
+//            return $result[$cryptoId];
+//        }
+
+        return $this->trades[$cryptoId];
+    }
+
+    public function test()
+    {
+        dump($this->trades);
+        dump($this->tradesKeys);
+        dump($this->collapseClasses);
     }
 
     public function render()
