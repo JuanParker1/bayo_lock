71a3b71f5386a0108514718542d81b2c9b55b54e
Omer-alba <o.albadry@foxxum.com>
2022-04-24 22:41:01 +0200

    [FIX] unkown html structure [FIX adding some clean code

--- resources/views/livewire/show-trade-childern.blade.php
+++ resources/views/livewire/show-trade-childern.blade.php
@@ -1,50 +1,52 @@
 <div>
-    <link href="{{ asset('css/trade-childern.css') }}" rel="stylesheet">
-    <center>
-        <div class="-table">
-            <div class="inline-block">
-                <div class="collective-children header">
-                    Order date
+    @if($ids)
+        <link href="{{ asset('css/trade-childern.css') }}" rel="stylesheet">
+        <center>
+            <div class="-table">
+                <div class="inline-block">
+                    <div class="collective-children header">
+                        Order date
+                    </div>
+                    <div class="collective-children header">
+                        Amount
+                    </div>
+                    <div class="collective-children header">
+                        Price
+                    </div>
+                    <div class="collective-children header">
+                        Location
+                    </div>
                 </div>
-                <div class="collective-children header">
-                    Amount
-                </div>
-                <div class="collective-children header">
-                    Price
-                </div>
-                <div class="collective-children header">
-                    Location
+
+                {{--     table body --}}
+                @foreach($collective as $element)
+                    <div class="inline-block collective-row">
+
+                        <div class="collective-children body">
+                            {!! date('d M. Y' ,strtotime($element["order-day"])) !!}
+                        </div>
+                        <div class="collective-children body">
+                            {!! $element["total-currency"] !!}
+                        </div>
+                        <div class="collective-children body">
+                            {!! $element['currency-single-price'] * $element["total-currency"] !!} Eur.
+                        </div>
+                        <div class="collective-children body">
+                            <span class="full-width" wire:click="openModal('{{ $element['id'] }}')"></span>
+                            Binance
+                        </div>
+
+                    </div>
+                @endforeach
+
+                {{--     table footer --}}
+                <div class="inline-block">
+                    <div class="collective-children footer"></div>
+                    <div class="collective-children footer"></div>
+                    <div class="collective-children footer"></div>
+                    <div class="collective-children footer"></div>
                 </div>
             </div>
-
-            {{--     table body --}}
-            @foreach($collective as $element)
-                <div class="inline-block collective-row">
-
-                    <div class="collective-children body">
-                        {!! date('d M. Y' ,strtotime($element["order-day"])) !!}
-                    </div>
-                    <div class="collective-children body">
-                        {!! $element["total-currency"] !!}
-                    </div>
-                    <div class="collective-children body">
-                        {!! $element['currency-single-price'] * $element["total-currency"] !!} Eur.
-                    </div>
-                    <div class="collective-children body">
-                        <span class="full-width" wire:click="openModal('{{ $element['id'] }}')"></span>
-                        Binance
-                    </div>
-
-                </div>
-            @endforeach
-
-            {{--     table footer --}}
-            <div class="inline-block">
-                <div class="collective-children footer"></div>
-                <div class="collective-children footer"></div>
-                <div class="collective-children footer"></div>
-                <div class="collective-children footer"></div>
-            </div>
-        </div>
-    </center>
+        </center>
+    @endif
 </div>
