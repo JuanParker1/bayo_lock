6b84752652599efaacfcb88ff86f567545dbea8d
Omer-alba <o.albadry@foxxum.com>
2022-04-24 22:43:48 +0200

    [FIX] rename variables to better and stronger title

--- resources/views/livewire/trade-modal.blade.php
+++ resources/views/livewire/trade-modal.blade.php
@@ -1,53 +1,84 @@
-<div class="blur-container" wire:click.prevent="$emitUp('closeModal')">
-    <div class="block">
+<div>
+    @if($showTradeInfo)
+        <div class="blur-container" wire:click.prevent="$emit('closeModal', true)">
+            <div class="block">
 
-        <div class="block-modal" wire:click.stop="">
-            <div class="block-modal-layout">
+                <div class="block-modal" wire:click.stop="">
+                    <div class="block-modal-layout">
 
-                <div class="modal-icon icon">
-                    <i class="bi bi-calendar-week"></i>
-                </div>
+                        <div class="modal-icon icon">
+                            <i class="bi bi-calendar-week"></i>
+                        </div>
 
-                <div class="modal-date">
-                    {{ date('d M Y', strtotime($trade['orderDay'])) }}
-                </div>
-                <div class="modal-content">content</div>
+                        <div class="modal-date">
+                            {{ date('d M Y', strtotime($trade['orderDay'])) }}
+                        </div>
 
-                <div class="modal-currentPrice">
-                    <div class="block">
-                        <div class="block-child seperation">Bought price</div>
-                        <div class="block-child seperation">
+                        {{-- Action --}}
+                        @if($trade['isCollective'])
+                            <div class="modal-action">
+                                <div class="block">
+
+                                    {{-- Bin --}}
+                                    <div class="block-child seperation">
+                                        <div class="action xx-large"
+                                        wire:click.prevent="delete" >
+                                            <i class="bi bi-trash3"></i>
+                                        </div>
+                                    </div>
+
+                                    {{-- edit --}}
+                                    <div class="block-child seperation">
+                                        <div class="action xx-large"
+                                             wire:click="openModal('edit','{{ $trade["cryptoId"] }}')">
+                                            <i class="bi bi-plus-slash-minus"></i>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        @endif
+
+                        {{-- Content --}}
+                        <div class="modal-content">content</div>
+
+                        <div class="modal-currentPrice">
+                            <div class="block">
+                                <div class="block-child seperation">Bought price</div>
+                                <div class="block-child seperation">
                                     <span>
                                         {!!  round($trade['currencySinglePrice'],2) !!}€
                                     </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="modal-buyPrice">
-                    <div class="block">
-                        <div class="block-child seperation">Current price</div>
-                        <div class="block-child seperation">
+                        <div class="modal-buyPrice">
+                            <div class="block">
+                                <div class="block-child seperation">Current price</div>
+                                <div class="block-child seperation">
                                     <span>
-                                        {!!  round($currentPrice,2) !!}€
+                                        {!!  round($livePrice,2) !!}€
                                     </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="modal-percent">
-                    <div class="block">
-                        <div class="block-child seperation">Balance</div>
-                        <div class="block-child seperation">
+                        <div class="modal-percent">
+                            <div class="block">
+                                <div class="block-child seperation">Balance</div>
+                                <div class="block-child seperation">
                                     <span>
-                                        {!! round($currentBalance['percentage'],2) !!}%
+                                        {!! round($liveBalance['percentage'],2) !!}%
                                     </span>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
                 </div>
 
             </div>
         </div>
-
-    </div>
+    @endif
 </div>
