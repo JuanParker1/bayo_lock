c1608ac9deb254fc3144d53559c1f3a59a121021
Omer-alba <o.albadry@foxxum.com>
2022-04-25 01:10:50 +0200

    [FIX] adding public function show

--- app/Http/Controllers/InvestorController.php
+++ app/Http/Controllers/InvestorController.php
@@ -8,12 +8,30 @@ use Illuminate\Http\Request;
 
 class InvestorController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function index()
     {
         $investors = Investor::all();
         return view('investor.index')->with([
             'investors' => $investors
         ]);
+    }
+
+    /**
+     * @param $investor_id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function show($investor_id)
+    {
+        $investor = Investor::find($investor_id);
+
+        return view('investor.show')
+            ->with([
+                'contracts' => $investor->contracts,
+                'investor' => $investor,
+            ]);
     }
 
     public function create()
@@ -28,17 +46,6 @@ class InvestorController extends Controller
             'lastname' => $request->get('lastname'),
             'telefon' => $request->get('telefon'),
         ]);
-    }
-
-    public function showContracts($investor_id)
-    {
-        $investor = Investor::find($investor_id);
-
-        return view('investor.contracts')
-            ->with([
-                'contracts' => $investor->contracts,
-                'investor' => $investor,
-            ]);
     }
 
     public function createContract($investor_id)
