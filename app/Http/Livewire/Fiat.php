33605701d70728fa9c0b93b345ba1382d74467b9
Omer-alba <o.albadry@foxxum.com>
2022-05-08 21:34:03 +0200

    [FIX] creating logic and view

--- app/Http/Livewire/Fiat.php
+++ app/Http/Livewire/Fiat.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\UserPreference;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Fiat extends Component
 {
+    public $preferredFiat = 'eur';
+    protected $listeners = ['getFiat'];
+
+    public function toggle($fiat)
+    {
+        if ($this->preferredFiat === $fiat) return null;
+        // die nicht ausgesuchte bitte in grau hinterlegen, die ausgesuchte in blau
+        $this->preferredFiat = $fiat;
+        $this->emitTo('trades.index', 'refreshFiat', $this->preferredFiat);
+        // speichere die ausgewählte fiat
+        dump(Auth::user());
+    }
+
+    public function getFiat()
+    {
+        return $this->preferredFiat;
+    }
+
     public function render()
     {
         return view('livewire.fiat');
--- resources/views/livewire/fiat.blade.php
+++ resources/views/livewire/fiat.blade.php
@@ -1,3 +1,8 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="width-10-percent">
+        <div class="block">
+            <div class="block-child seperation {!! $preferredFiat === 'eur' ? 'highlight' : 'discreet' !!}" wire:click="toggle('eur')">euro</div>
+            <div class="block-child seperation {!! $preferredFiat === 'usd' ? 'highlight' : 'discreet' !!}" wire:click="toggle('usd')">usd</div>
+        </div>
+    </div>
 </div>
